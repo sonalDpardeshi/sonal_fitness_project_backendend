a20e7a70c8e2e0f51a9c4ecd6172ec5f16c852b5
Sonal Dilip Pardeshi <78472348+sonalDpardeshi@users.noreply.github.com>
2025-03-27 12:03:25 +0530

    new changes in main file
    pushed by Sonal

--- Personalised_Fitness_Prediction_System/src/main/java/com/example/demo/PersonalisedFitnessPredictionSystemApplication.java
+++ Personalised_Fitness_Prediction_System/src/main/java/com/example/demo/PersonalisedFitnessPredictionSystemApplication.java
@@ -8,6 +8,7 @@ public class PersonalisedFitnessPredictionSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalisedFitnessPredictionSystemApplication.class, args);
+	//new comment
 	}
 
 }
