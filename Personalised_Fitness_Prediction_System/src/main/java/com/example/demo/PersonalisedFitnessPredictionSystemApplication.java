0fa798deea7aeb489c218f41d08eca950e3a057d
Sonal Dilip Pardeshi <78472348+sonalDpardeshi@users.noreply.github.com>
2025-03-27 12:01:05 +0530

    project added to main branch
    code pushed by Sonal

---
+++ Personalised_Fitness_Prediction_System/src/main/java/com/example/demo/PersonalisedFitnessPredictionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalisedFitnessPredictionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalisedFitnessPredictionSystemApplication.class, args);
+	}
+
+}
